63598ceabf515b53e659e8d5221b7453f2aa8f87
Samira Pessoal <sixoobecan@gmail.com>
2022-07-20 09:28:48 -0300

    Dentro de scr

--- ProjetoClasse/Program.cs
+++ ProjetoClasse/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using static System.Console;
 
-namespace ProjetoClasse.scr
+namespace ProjetoClasse
 {
     internal class Program
     {
@@ -26,37 +26,7 @@ namespace ProjetoClasse.scr
         }
     }
 }
-namespace ProjetoClasse.src
-{
 
 
-    public class Vestido
-    {
-        public string Tecido { get; set; }
-        public string Linha { get; set; }
-        public string Botoes { get; set; }
-        public string Etiqueta { get; set; }
 
-        public Vestido()
-        {
-        }
-
-        public Vestido(string tecido, string linha, string botoes, string etiqueta)
-        {
-            Tecido = tecido;
-            Linha = linha;
-            Botoes = botoes;
-            Etiqueta = etiqueta;
-
-        }
-        public override string ToString()
-        {
-            return $"Materia: {Tecido};\n" +
-                $"Aviamento: {Linha};\n" +
-                $"Acabamento: {Botoes};\n" +
-                $"Marca: {Etiqueta};\n";
-
-        }
-    }
-
-}
+   
---
+++ ProjetoClasse/src/Vestido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoClasse.src
+{
+    public class Vestido
+    {
+        public string Tecido { get; set; }
+        public string Linha { get; set; }
+        public string Botoes { get; set; }
+        public string Etiqueta { get; set; }
+
+        public Vestido()
+        {
+        }
+
+        public Vestido(string tecido, string linha, string botoes, string etiqueta)
+        {
+            Tecido = tecido;
+            Linha = linha;
+            Botoes = botoes;
+            Etiqueta = etiqueta;
+
+        }
+        public override string ToString()
+        {
+            return $"Materia: {Tecido};\n" +
+                $"Aviamento: {Linha};\n" +
+                $"Acabamento: {Botoes};\n" +
+                $"Marca: {Etiqueta};\n";
+
+        }
+    }
+
+}
